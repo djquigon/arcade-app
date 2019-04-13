16c96b22a07c67c1f48317077805adc6be6e6be9
Logan Scheid <lds96184@uga.edu>
2019-04-13 13:20:23 -0400

    added new methods to MainMenu to reduce length of constructor

--- src/main/java/cs1302/arcade/MainMenu.java
+++ src/main/java/cs1302/arcade/MainMenu.java
@@ -12,6 +12,7 @@ import javafx.scene.control.Button;
 import javafx.geometry.Insets;
 import javafx.scene.control.Separator;
 import javafx.geometry.Orientation;
+import javafx.scene.text.Text;
 
 public class MainMenu extends VBox{
 
@@ -21,32 +22,46 @@ public class MainMenu extends VBox{
 
     public MainMenu(){
         super();
-
+        
         //create menuBar
         menuBar = new MenuBar();
-        Menu file = new Menu("File");
-        MenuItem exit = new MenuItem("Exit");
-        file.getItems().add(exit);
-        menuBar.getMenus().addAll(file);
-
+        this.setMenuBar(menuBar);
+        
         //create games
         games = new HBox();
+        this.setGames(games);
+        
+        //create bottomBar
+        //bottomBar = new HBox();
+        //this.setBottomBar(bottomBar);
+        
+        //add all components to layout
+        this.getChildren().addAll(menuBar, games); //bottombar
+    }
+
+    public void setMenuBar(MenuBar menuBar){
+        Menu file = new Menu("File");
+        MenuItem exit = new MenuItem("Exit");
+        exit.setOnAction(e-> System.exit(0));
+        file.getItems().add(exit);
+        menuBar.getMenus().addAll(file);
+    }
+    
+    public void setGames(HBox games){
         VBox game1 = new VBox();
         VBox game2 = new VBox();
-        ImageView iv1 = new ImageView(); //remeber to set this to image1
-        ImageView iv2 = new ImageView(); //remeber to set this to image2
-        Image i1 = new Image("http://cobweb.cs.uga.edu/~mec/cs1302/gui/pikachu.png"); //pass in picture of game 1
-        Image i2 = new Image("http://cobweb.cs.uga.edu/~mec/cs1302/gui/pikachu.png"); //pass in picture of game 2
+        ImageView iv1 = new ImageView();
+        ImageView iv2 = new ImageView(); 
+        Image i1 = new Image("http://cobweb.cs.uga.edu/~mec/cs1302/gui/pikachu.png");
+        Image i2 = new Image("http://cobweb.cs.uga.edu/~mec/cs1302/gui/pikachu.png");
         iv1.setImage(i1);
         iv2.setImage(i2);
         Button b1 = new Button("Play: (insert game)");
         b1.setMaxSize(200, 1000);
         Button b2 = new Button("Play: (insert game)");
         b2.setMaxSize(200, 1000);
-
         Separator sep1 = new Separator(Orientation.HORIZONTAL);
         Separator sep2 = new Separator(Orientation.HORIZONTAL);
-        
         game1.getChildren().addAll(iv1, b1, sep1);
         game2.getChildren().addAll(iv2, b2, sep2);
         game1.setMargin(iv1, new Insets(50));
@@ -54,11 +69,10 @@ public class MainMenu extends VBox{
         game2.setMargin(iv2, new Insets(50));
         game2.setMargin(b2, new Insets(0, 0, 50, 100));
         games.getChildren().addAll(game1, game2);
-        
-        //create bottomBar
-        //bottomBar = new HBox();
+    }
 
-        //add all components to layout
-        this.getChildren().addAll(menuBar, games); //bottomBar
+    public void setBottomBar(HBox bottomBar){
+        Text credit = new Text("Courtesy of team Lano");
+        bottomBar.getChildren().addAll(credit);
     }
 }
