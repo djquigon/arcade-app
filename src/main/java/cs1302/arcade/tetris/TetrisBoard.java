bb6a72334673b11accdc8aa292c688bfd9111285
Ano Karadaghi <aak17849@uga.edu>
2019-04-18 02:27:56 -0400

    Experimented with board

--- src/main/java/cs1302/arcade/tetris/TetrisBoard.java
+++ src/main/java/cs1302/arcade/tetris/TetrisBoard.java
@@ -1,26 +1,41 @@
 package cs1302.arcade;
 
-public class TetrisBoard{
-    
-    public final int HEIGHT = 20;
-    public final int WIDTH = 10;
-    public int[][] matrix;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
 
+
+//Commented out everything for now to test functionality
+public class TetrisBoard{
+
+    //public final int HEIGHT = 20;
+    //public final int WIDTH = 10;
+    //public int[][] matrix;
+    
+/*    public TetrisBoard(){
+        Rectangle[][] arr = new Rectangle[HEIGHT][WIDTH];
+        for(int i = 0; i < HEIGHT; i++){
+            for(int j = 0; j < WIDTH; j++){
+                Rectangle r = new Rectangle(7,7,Color.BLACK);
+                arr[i][j] = r;
+                
+            }
+        }
+        }*/
     /**
      *Creates a 2D array representing the tiles of the tetris board.
      */
-    public TetrisBoard() {
+    /* public TetrisBoard() {
         matrix = new int[HEIGHT][WIDTH];
         for (int i = 0; i< HEIGHT; i++){
             for (int j = 0; j < WIDTH; j++){
                 matrix[i][j] = 0;
             }//for
         }//for
-    }//Board
+        }//Board*/
     
-    public int getBlock(int x, int y) {
+    /*public int getBlock(int x, int y) {
         return matrix[y][x];
-    }//Block
+        }//Block*/
 
     /*public int getHeight(){
         return this.HEIGHT;
--- src/main/java/cs1302/arcade/tetris/TetrisStage.java
+++ src/main/java/cs1302/arcade/tetris/TetrisStage.java
@@ -14,6 +14,7 @@ import javafx.event.EventHandler;
 import javafx.scene.input.KeyEvent;
 import cs1302.arcade.*;
 //
+import javafx.scene.layout.*;
 import javafx.scene.canvas.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.*;
@@ -24,39 +25,63 @@ public class TetrisStage extends Stage{
     
     Group layout;
     MenuBar menuBar;
+
+    public final int HEIGHT = 20;
+    public final int WIDTH = 10;
     private final Color[] colors = {Color.WHITE, Color.ORANGE, Color.GREEN, Color.CYAN, Color.MAGENTA, Color.BLUE, Color.YELLOW, Color.RED};
     //private static final int X_OFFSET = 20, Y_OFFSET = 20;
     /* private static final int X_OFFSET = 20, Y_OFFSET = 20, NEXTPIECEX = 360, NEXTPIECEY = 40;
     private static final int HEIGHT = 680, WIDTH = 640, BLOCK_SIZE = 32;
     private static int level = 1;
     private static double score;
-    private static int lines;*/
+    private static int lines;
     Canvas canvas = new Canvas(WIDTH, HEIGHT);
     GraphicsContext gc = canvas.getGraphicsContext2D();
-    
+    */    
 
-    TetrisBoard board = new TetrisBoard();
+    HBox hb = new HBox();
+    VBox vb = new VBox();
+    VBox vb2 = new VBox();
     
+    //TetrisBoard board = new TetrisBoard();
     public TetrisStage(){
         super();
-        //gc.setFill(Color.RED);
-        //gc.fillRect(5,5,1,1);
-        //board.matrix[0][0] = Color.RED; // top-left is red
-        //board.matrix[0][5] = Color.BLUE; // top-right is white
-        //board.matrix[5][0] = Color.GREEN; // bottom-left is green
-        //board.matrix[5][5] = Color.GRAY; // bottom-right is gray
-        layout = new Group();
-        //drawBoard(gc);
-        this.setMenuBar();
-        layout.getChildren().addAll(menuBar);
-        Scene window = new Scene(layout, 800, 680);
-        this.initModality(Modality.APPLICATION_MODAL);
-        this.setTitle("T e t r i s");
-        this.setMinWidth(800);
-        this.setMinHeight(680);
-        this.setResizable(false);
-        this.sizeToScene();
-        this.setScene(window);
+        //This should go in board class just experimenting here
+        Rectangle[][] rect = new Rectangle[HEIGHT][WIDTH];
+        for (int i = 0; i < HEIGHT; i++) {
+            hb = new HBox();
+            for (int j = 0; j < WIDTH; j++) {
+                rect[i][j] = new Rectangle(35.0,35.0, Color.TRANSPARENT);
+                rect[i][j].setStroke(Color.BLACK);
+                hb.getChildren().add(rect[i][j]);
+            }
+            vb.getChildren().add(hb);
+        }
+
+        //Gonna need this so seperate to call multiple times
+        
+        /*for (int i = 0; i < rect.length; i++) {
+            hBox = new HBox();
+            for (int j = 0; j < rect[0].length; j++) {
+                hBox.getChildren().add(rect[i][j]);
+            }
+            vBox.getChildren().add(hBox);
+
+            }*/
+       
+         //
+         layout = new Group();
+         //drawBoard(gc);
+         this.setMenuBar();
+         //layout.getChildren().addAll(menuBar,vb);
+         Scene window = new Scene(vb, 800, 680);
+         this.initModality(Modality.APPLICATION_MODAL);
+         this.setTitle("T e t r i s");
+         this.setMinWidth(800);
+         this.setMinHeight(680);
+         this.setResizable(false);
+         this.sizeToScene();
+         this.setScene(window);
     }
 
     public void setMenuBar(){
