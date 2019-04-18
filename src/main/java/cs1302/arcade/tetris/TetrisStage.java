37af79f3e416437e6ab1f7a9b04f84d7617c6e0c
Ano Karadaghi <aak17849@uga.edu>
2019-04-18 00:52:58 -0400

    Commented out some lines

--- src/main/java/cs1302/arcade/tetris/TetrisStage.java
+++ src/main/java/cs1302/arcade/tetris/TetrisStage.java
@@ -26,11 +26,11 @@ public class TetrisStage extends Stage{
     MenuBar menuBar;
     private final Color[] colors = {Color.WHITE, Color.ORANGE, Color.GREEN, Color.CYAN, Color.MAGENTA, Color.BLUE, Color.YELLOW, Color.RED};
     //private static final int X_OFFSET = 20, Y_OFFSET = 20;
-    private static final int X_OFFSET = 20, Y_OFFSET = 20, NEXTPIECEX = 360, NEXTPIECEY = 40;
+    /* private static final int X_OFFSET = 20, Y_OFFSET = 20, NEXTPIECEX = 360, NEXTPIECEY = 40;
     private static final int HEIGHT = 680, WIDTH = 640, BLOCK_SIZE = 32;
     private static int level = 1;
     private static double score;
-    private static int lines;
+    private static int lines;*/
     Canvas canvas = new Canvas(WIDTH, HEIGHT);
     GraphicsContext gc = canvas.getGraphicsContext2D();
     
@@ -46,9 +46,9 @@ public class TetrisStage extends Stage{
         //board.matrix[5][0] = Color.GREEN; // bottom-left is green
         //board.matrix[5][5] = Color.GRAY; // bottom-right is gray
         layout = new Group();
-        drawBoard(gc);
+        //drawBoard(gc);
         this.setMenuBar();
-        layout.getChildren().addAll(menuBar,canvas);
+        layout.getChildren().addAll(menuBar);
         Scene window = new Scene(layout, 800, 680);
         this.initModality(Modality.APPLICATION_MODAL);
         this.setTitle("T e t r i s");
@@ -72,7 +72,7 @@ public class TetrisStage extends Stage{
         menuBar.prefWidthProperty().bind(this.widthProperty());
     }
     
-    private void drawBoard(GraphicsContext g2d) {
+    /*private void drawBoard(GraphicsContext g2d) {
         // g2d.setFont(new Font("Comic Sans MS", 20));
         g2d.setFill(Color.GRAY);
         g2d.fillRect(0, 0, WIDTH, HEIGHT);
@@ -86,7 +86,7 @@ public class TetrisStage extends Stage{
                 g2d.setFill(colors[board.getBlock(i, j)]);
                 g2d.fillRect(X_OFFSET+i*BLOCK_SIZE, Y_OFFSET+j*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
             }
-    }// drawBoard
+            }// drawBoard*/
     
     
 }
