e82c1d61fa40d764cee4f78e94cf452e6c8b6828
Logan Scheid <lds96184@uga.edu>
2019-04-19 17:42:42 -0400

    Simple moving animation now working for L block, rearranged some of the scene graph

--- src/main/java/cs1302/arcade/tetris/TetrisStage.java
+++ src/main/java/cs1302/arcade/tetris/TetrisStage.java
@@ -22,36 +22,45 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.*;
 import javafx.util.Duration;
+import javafx.scene.control.Separator;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 //
 
 public class TetrisStage extends Stage{
 
-    public final int HEIGHT = 20;
-    public final int WIDTH = 10;
+    public static final int HEIGHT = 20;
+    public static final int WIDTH = 10;
     
     EventHandler<ActionEvent> handler;
-    Group layout;
+    Scene scene;
     MenuBar menuBar;
     KeyFrame keyFrame;
     Timeline timeline;
     int iteration = 1;
-    HBox hb = new HBox();
-    VBox vb = new VBox();
-    VBox vb2 = new VBox();
+    int scoreTotal;
+    Text score;
+    HBox main;
+    VBox board;
+    VBox window;
+    VBox info;
+    Separator sep;
     Rectangle[][] rect;
     
     public TetrisStage(){
         super();
+        this.setMenuBar(); //initializes menuBar
         //This should go in board class just experimenting here
         rect = new Rectangle[HEIGHT][WIDTH];
+        board = new VBox();
         for (int i = 0; i < HEIGHT; i++) {
-            hb = new HBox();
+            HBox row = new HBox();
             for (int j = 0; j < WIDTH; j++) {
                 rect[i][j] = new Rectangle(30.0,30.0, Color.TRANSPARENT);
                 rect[i][j].setStroke(Color.BLACK);
-                hb.getChildren().add(rect[i][j]);
+                row.getChildren().add(rect[i][j]);
             }
-            vb.getChildren().add(hb);
+            board.getChildren().add(row);
         }    
 
         //Gonna need this so seperate to call multiple times
@@ -64,20 +73,39 @@ public class TetrisStage extends Stage{
             vBox.getChildren().add(hBox);
 
             }*/
-       
-         this.setMenuBar();
-         //This fixes menubar
-         vb2.getChildren().addAll(menuBar, vb);
-         vb2.setMargin(vb, new Insets(10,0,0,75));
-         
-         Scene window = new Scene(vb2, 800, 680);
+        window = new VBox();
+        main = new HBox();
+        //board
+        sep = new Separator(Orientation.VERTICAL);
+        scoreTotal = 0;
+        score = new Text("Score: " + scoreTotal);
+        score.setFont(new Font(30));
+        info = new VBox();
+        info.setAlignment(Pos.TOP_CENTER);
+        info.getChildren().addAll(score);
+        main.getChildren().addAll(board, sep, info);
+        window.getChildren().addAll(menuBar, main);
+        main.setMargin(board, new Insets(10,75,0,75));
+        /*
+         window = new VBox();
+         window.getChildren().addAll(menuBar, board);
+         window.setMargin(vb, new Insets(10,50,0,75));
+         sep = new Separator(Orientation.VERTICAL);
+         scoreTotal = 0;
+         score = new Text("Score: " + scoreTotal);
+         main = new HBox();
+         VBox info = new VBox();
+         info.getChildren().addAll(score);
+         main.getChildren().addAll(window, sep, info);
+        */
+         scene = new Scene(window, 800, 680);
          this.initModality(Modality.APPLICATION_MODAL);
          this.setTitle("T e t r i s");
          this.setMinWidth(800);
          this.setMinHeight(680);
          this.setResizable(false);
          this.sizeToScene();
-         this.setScene(window);
+         this.setScene(scene);
          this.blockMove();
     }
 
@@ -85,40 +113,40 @@ public class TetrisStage extends Stage{
          handler = new EventHandler<ActionEvent>(){
                  @Override
                  public void handle(ActionEvent event){
-                     if(iteration > HEIGHT - 1){
-                         iteration = 0;
+                     if(iteration == HEIGHT - 1){
+                         iteration = 1;
+                         return;
                      }
-                     for(int i = iteration; i < i+1; i++){
+                     int currIteration = iteration;
+                     for(int i = iteration; i < currIteration+1; i++){
                          //Filling
-                         
                          for(int j = 4; j< 7; j++){
                              if(j == 4){
                                  rect[i-1][j].setFill(Color.RED);
                              }
                              rect[i][j].setFill(Color.RED);
                          }//for j
-
                          //Animation
                          for(int w = 6; w > 3; w--){
                              //Moving down
                              rect[i+1][w].setFill(Color.RED);
                              if(w == 4){
-                                 //rect[i-1][w].setFill(Color.TRANSPARENT);
+                                 rect[i-1][w].setFill(Color.TRANSPARENT);
                                  //Preserving 2 value
                                  break;
                              }
                                 //Removing above
-                             //rect[i][w].setFill(Color.TRANSPARENT);
-                         }//for white
+                             rect[i][w].setFill(Color.TRANSPARENT);
+                             }//for white
                          iteration++;
                      }//for i
                  }//handle
              };//EventHandler
-         
          //Intializes KeyFrame with animation being done every 2 seconds         
-         keyFrame = new KeyFrame(Duration.millis(2500), handler);
+         keyFrame = new KeyFrame(Duration.millis(1500), handler);
          timeline = new Timeline();
          timeline.setCycleCount(Timeline.INDEFINITE);
+         
          timeline.getKeyFrames().add(keyFrame);
          timeline.play();
      }
@@ -127,7 +155,10 @@ public class TetrisStage extends Stage{
         menuBar = new MenuBar();
         Menu file = new Menu("File");
         MenuItem exit = new MenuItem("Exit to Main Menu");
-        exit.setOnAction(e -> this.close());
+        exit.setOnAction(e -> {
+                this.close();
+                timeline.stop();
+            });
         file.getItems().add(exit);
         Menu help = new Menu("Help");
         MenuItem about = new MenuItem("How to Play");
